4aa8494c4c9e2defaacc3259c6c143e667537fe0
Maya <95750274+mnj03@users.noreply.github.com>
2022-02-09 22:28:02 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
 // http://api.openweathermap.org/
-
-
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Moment from 'react-moment';
+import 'moment-timezone';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [weather, cWeather] = useState({
+    day: "",
+    date: "",
+    city: "",
+  });
+
+ const [fetching] = useState(true);
+ 
+
+ const [day, cDay] = useState([]);
+
+  const updateWeather = (jsonResponse) => {
+    cWeather({
+      day: jsonResponse.day,
+      date: jsonResponse.date,
+      city: jsonResponse.city
+    });
+  };
+
+  const status = (jsonResponse) => {
+    if (jsonResponse.status >= 200 && jsonResponse.status < 300) {
+      return Promise.resolve(jsonResponse);
+    } else {
+      return Promise.reject(new Error(jsonResponse.statusText));
+    }
+  }
+
+    fetching(true);{
+    axios
+      .get("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=8c2a096453fb47ade71a65fb1354cd91")
+      
+      
+      .then(status)
+      .then((res) => {
+        updateWeather(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally((useState) => fetching(false));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <h1>Weather</h1>
+      <main>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>DAY:</b> {weather.day}{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <b>DATE:</b> {weather.date}{" "}
+        </p>
+        <p>
+          <b>CITY:</b> {weather.city}{" "}
+        </p>
+      </main>
+
+      <footer>
+      </footer>
+
+
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Moment from 'react-moment';
+import 'moment-timezone';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
